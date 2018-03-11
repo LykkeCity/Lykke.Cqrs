7e45248b8c6e9056ef15902ae1fd24e38f26d3f1
Mikhail Musin <mikhail.musin@gmail.com>
2018-03-12 07:03:38 +0800

    made CqrsEngine.PublishEvent public (temporary)

--- src/Lykke.Cqrs/Abstractions/ICqrsEngine.cs
+++ src/Lykke.Cqrs/Abstractions/ICqrsEngine.cs
@@ -3,5 +3,6 @@
     public interface ICqrsEngine
     {
         void SendCommand<T>(T command, string boundedContext, string remoteBoundedContext, uint priority = 0);
+        void PublishEvent(object @event, string boundedContext);
     }
 }
