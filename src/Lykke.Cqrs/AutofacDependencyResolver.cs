0cf29fe53e3cc44d5bf13cbca75aeab8897f4ea4
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-07-24 20:05:15 +0300

    Added AutofacDependencyResolver

---
+++ src/Lykke.Cqrs/AutofacDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using JetBrains.Annotations;
+using System;
+
+namespace Lykke.Cqrs
+{
+    [PublicAPI]
+    public class AutofacDependencyResolver : IDependencyResolver
+    {
+        private readonly IComponentContext _context;
+
+        public AutofacDependencyResolver([NotNull] IComponentContext kernel)
+        {
+            _context = kernel ?? throw new ArgumentNullException(nameof(kernel));
+        }
+
+        public object GetService(Type type)
+        {
+            return _context.Resolve(type);
+        }
+
+        public bool HasService(Type type)
+        {
+            return _context.IsRegistered(type);
+        }
+    }
+}
