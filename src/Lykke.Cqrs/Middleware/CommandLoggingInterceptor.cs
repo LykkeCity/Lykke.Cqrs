0afa5dfcad4cbb9051449d26ac68e1faa5dd96d9
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-12-05 16:29:28 +0300

    Code review fixes

--- src/Lykke.Cqrs/Middleware/CommandLoggingInterceptor.cs
+++ src/Lykke.Cqrs/Middleware/CommandLoggingInterceptor.cs
@@ -1,41 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Common;
 using Common.Log;
 using Lykke.Common.Log;
 using Lykke.Cqrs.Abstractions.Middleware;
 
 namespace Lykke.Cqrs.Middleware
 {
+    public delegate void CommandLoggingDelegate(ILog log, object handlerObject, object command);
+
+    /// <summary>
+    /// Logging interceptor for commands.
+    /// </summary>
     public class CommandLoggingInterceptor : ICommandInterceptor
     {
         private readonly ILog _log;
-        private readonly Dictionary<Type, Action<ILog, object, object>> _customLoggingActionsMap;
+        private readonly Dictionary<Type, CommandLoggingDelegate> _customLoggingActionsMap;
 
+        /// <summary>
+        /// C-tor for old logging.
+        /// </summary>
         [Obsolete]
-        public CommandLoggingInterceptor(ILog log, Dictionary<Type, Action<ILog, object, object>> customLoggingActionsMap = null)
+        public CommandLoggingInterceptor(ILog log, Dictionary<Type, CommandLoggingDelegate> customLoggingActionsMap = null)
         {
             _log = log;
             _customLoggingActionsMap = customLoggingActionsMap;
         }
 
-        public CommandLoggingInterceptor(ILogFactory logFactory, Dictionary<Type, Action<ILog, object, object>> customLoggingActionsMap = null)
+        /// <summary>
+        /// C-tor.
+        /// </summary>
+        public CommandLoggingInterceptor(ILogFactory logFactory, Dictionary<Type, CommandLoggingDelegate> customLoggingActionsMap = null)
         {
             _log = logFactory.CreateLog(this);
             _customLoggingActionsMap = customLoggingActionsMap;
         }
 
+        /// <inheritdoc cref="ICommandInterceptor"/>
         public Task<CommandHandlingResult> InterceptAsync(ICommandInterceptionContext context)
         {
-            var commandType = context.Command.GetType();
-            Action<ILog, object, object> loggingAction = null;
-            if (_customLoggingActionsMap?.TryGetValue(commandType, out loggingAction) ?? false)
+            CommandLoggingDelegate loggingAction = null;
+            if (_customLoggingActionsMap?.TryGetValue(context.Command.GetType(), out loggingAction) ?? false)
                 loggingAction?.Invoke(_log, context.HandlerObject, context.Command);
             else
-                _log.WriteInfo(context.HandlerObject.GetType().Name, context.Command?.ToJson(), commandType.Name);
+                DefaultLog(_log, context.HandlerObject, context.Command);
 
             return Task.FromResult(CommandHandlingResult.Ok());
+        }
+
+        /// <summary>
+        /// Default logging for commands.
+        /// </summary>
+        /// <param name="log">ILog instance.</param>
+        /// <param name="handlerObject">Command handler instance.</param>
+        /// <param name="command">Command.</param>
+        /// <remarks>This could be used in case some custom logging interceptor is implemented for default cases.</remarks>
+        public static void DefaultLog(ILog log, object handlerObject, object command)
+        {
+            log.WriteInfo(handlerObject.GetType().Name, command, command.GetType().Name);
         }
     }
 }
--- src/Lykke.Cqrs/Middleware/EventLoggingInterceptor.cs
+++ src/Lykke.Cqrs/Middleware/EventLoggingInterceptor.cs
@@ -1,41 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Common;
 using Common.Log;
 using Lykke.Common.Log;
 using Lykke.Cqrs.Abstractions.Middleware;
 
 namespace Lykke.Cqrs.Middleware
 {
+    public delegate void EventLoggingDelegate(ILog log, object handlerObject, object @event);
+
+    /// <summary>
+    /// Logging interceptor for events.
+    /// </summary>
     public class EventLoggingInterceptor : IEventInterceptor
     {
         private readonly ILog _log;
-        private readonly Dictionary<Type, Action<ILog, object, object>> _customLoggingActionsMap;
+        private readonly Dictionary<Type, EventLoggingDelegate> _customLoggingActionsMap;
 
+        /// <summary>
+        /// C-tor for old logging.
+        /// </summary>
         [Obsolete]
-        public EventLoggingInterceptor(ILog log, Dictionary<Type, Action<ILog, object, object>> customLoggingActionsMap = null)
+        public EventLoggingInterceptor(ILog log, Dictionary<Type, EventLoggingDelegate> customLoggingActionsMap = null)
         {
             _log = log;
             _customLoggingActionsMap = customLoggingActionsMap;
         }
 
-        public EventLoggingInterceptor(ILogFactory logFactory, Dictionary<Type, Action<ILog, object, object>> customLoggingActionsMap = null)
+        /// <summary>
+        /// C-tor
+        /// </summary>
+        public EventLoggingInterceptor(ILogFactory logFactory, Dictionary<Type, EventLoggingDelegate> customLoggingActionsMap = null)
         {
             _log = logFactory.CreateLog(this);
             _customLoggingActionsMap = customLoggingActionsMap;
         }
 
+        /// <inheritdoc cref="IEventInterceptor"/>
         public Task<CommandHandlingResult> InterceptAsync(IEventInterceptionContext context)
         {
-            var eventType = context.Event.GetType();
-            Action<ILog, object, object> loggingAction = null;
-            if (_customLoggingActionsMap?.TryGetValue(eventType, out loggingAction) ?? false)
+            EventLoggingDelegate loggingAction = null;
+            if (_customLoggingActionsMap?.TryGetValue(context.Event.GetType(), out loggingAction) ?? false)
                 loggingAction?.Invoke(_log, context.HandlerObject, context.Event);
             else
-                _log.WriteInfo(context.HandlerObject.GetType().Name, context.Event?.ToJson(), eventType.Name);
+                DefaultLog(_log, context.HandlerObject, context.Event);
 
             return Task.FromResult(CommandHandlingResult.Ok());
+        }
+
+        /// <summary>
+        /// Default logging for events.
+        /// </summary>
+        /// <param name="log">ILog instance.</param>
+        /// <param name="handlerObject">Event handler instance.</param>
+        /// <param name="event">Event.</param>
+        /// <remarks>This could be used in case some custom logging interceptor is implemented for default cases.</remarks>
+        public static void DefaultLog(ILog log, object handlerObject, object @event)
+        {
+            log.WriteInfo(handlerObject.GetType().Name, @event, @event.GetType().Name);
         }
     }
 }
